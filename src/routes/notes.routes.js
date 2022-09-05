1be5613ea2155ee19cf8b21e36b171ad9e31cfb4
Gonzalo <gonzalosantacruzariel@gmail.com>
2022-09-05 00:57:22 -0300

    mas y mas routes

---
+++ src/routes/notes.routes.js
@@ -0,0 +1,29 @@
+const { Router } = require("express");
+const router = Router();
+
+const {
+  renderNoteForm,
+  createNewNote,
+  renderNotes,
+  renderEditForm,
+  updateNote,
+  deleteNote
+} = require("../controllers/notes.controller");
+
+// New Notes
+router.get("/notes/add", renderNoteForm);
+
+router.post("/notes/add", createNewNote);
+
+// Get All Note
+router.get("/notes", renderNotes);
+
+// Edit Notes
+router.get("/notes/edit/:id", renderEditForm);
+
+router.post("/notes/edit/:id", updateNote);
+
+// Delete Notes
+router.delete("/notes/delete/:id", deleteNote);
+
+module.exports = router;
